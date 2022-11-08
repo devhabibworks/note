a3765a63def0f082d4c5e5d2dfe0eaddc7c52ba4
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-08 13:23:24 +0300

    commit message

---
+++ lib/model/note.dart
@@ -0,0 +1,24 @@
+class Note {
+  var id;
+  var title;
+  var body;
+  var date;
+
+  Note({this.id, this.title, this.body, this.date});
+
+  Note.fromJson(Map<String, dynamic> map) {
+    id = map['id'];
+    title = map['title'];
+    body = map['body'];
+    date = map['date'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "body": body,
+      "date": date,
+    };
+  }
+}
