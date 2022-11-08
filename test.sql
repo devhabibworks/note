538c577026db7390cacca06c4318a87f3126b337
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-08 13:31:01 +0300

    first

---
+++ test.sql
@@ -0,0 +1,30 @@
+-- code used to create table 
+
+CREATE TABLE if noT EXISTS note(
+  
+   ID INTEGER  PRIMARY KEY  AUTOINCREMENT , 
+   TITLE TEXT,
+   BODY TEXT NOT NULL,
+   DATE TEXT NOT NULL
+);
+
+--code used to insert new values
+INSERT into note VALUES( null, "titl1" , "body1" , "date1");
+
+--select all values 
+SELECT * from note;
+
+--select with connition
+SELECT * from note WHERE id = 1;
+
+--update table
+UPDATE note set 
+body = "Nawaf is late"
+WHERE id = 1;
+
+-- delet value from table
+DELETE FROM note
+WHERE id = 1;
+
+--delete column from table
+ALTER TABLE TABLENAME DROP COLUMN COLUMNNAME
