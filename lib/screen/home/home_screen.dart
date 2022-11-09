d7ae191c51c53335d158844fbc5dfebcf1218d6d
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-09 14:31:27 +0300

    floating

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note/databse/sql_helper.dart';
 
@@ -16,7 +17,39 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notes"),
-        actions: const [Icon(Icons.search), Icon(Icons.settings)],
+        actions: const [
+          Icon(
+            CupertinoIcons.search,
+            size: 30,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Icon(
+            Icons.settings,
+            size: 30,
+          ),
+          SizedBox(
+            width: 16,
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.pushNamed(context, "add-note");
+        },
+        label: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 3),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(10)),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+        extendedPadding: const EdgeInsets.symmetric(horizontal: 28),
       ),
     );
   }
