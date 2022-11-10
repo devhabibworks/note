c61f79daf4043b84ffd2fd08fc355d5b1453bed7
habibTeck <abdualhabibabdualrahman@gmail.com>
2022-11-10 14:33:57 +0300

    add-note

--- lib/screen/add-note/add_note_screen.dart
+++ lib/screen/add-note/add_note_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class AddNoteScreen extends StatelessWidget {
+import '../../model/note.dart';
+
+class AddNoteScreen extends StatefulWidget {
   const AddNoteScreen({Key? key}) : super(key: key);
 
   @override
+  State<AddNoteScreen> createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  late TextEditingController titleContoller;
+  late TextEditingController bodyController;
+  final _formKey = GlobalKey<FormState>();
+  Note note = Note();
+
+  @override
+  void initState() {
+    titleContoller = TextEditingController(text: "");
+    bodyController = TextEditingController(text: "");
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Note"),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(),
+              TextFormField(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
